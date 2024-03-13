d1b83ad2c701b895bee88a7d8e1c631d37b706b0
kennylouis29 <125714859+kennylouis29@users.noreply.github.com>
2024-03-13 16:24:39 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,56 @@
+<html>
+
+<head>
+<meta charset = "utf-8">
+<meta name = "CV">
+<title> CV </title>
+</head>
+
+<body>
+<center><font size ="30" color = "black"> CV </font></center>
+<h1> Personal Data </h1>
+
+<table color ="white"> 
+<img src = "PASFOTO.jpg" width = "100" height = "20%" border = "1" alt = "pasfoto">
+<tr><td> Name </td><td> : </td><td> Kenny Louis </td></tr>
+<tr><td> Date of birth </td><td> : </td><td> 3 january 2005 </td></tr>
+<tr><td> Gender </td><td> : </td><td> Male </td></tr>
+<tr><td> Height </td><td> : </td><td> 170 cm </td></tr>
+<tr><td> Weight </td><td> : </td><td> 56 kg </td></tr>
+<tr><td> Nationality </td><td> : </td><td> Indonesian </td></tr>
+</table>
+
+<hr width ="100%" color ="black">
+
+<h2><font size = "5" color = "black">EDUCATIONAL BACKGROUND</font></h2>
+<center>
+<table border="1" bgcolor = "yellow">
+<tr bgcolor ="green"><th>Period</th><th>School</th><th>Major</th></tr>
+<tr><td>2010-2016</td><td>Sutomo 1 Elementary School</td><td>-</td></tr>
+<tr><td>2016-2019</td><td>Sutomo 1 Junior High School</td><td>-</td></tr>
+<tr><td>2019-2022</td><td>SMA Sutomo 1 Medan</td><td>Social Science</td></tr>
+<tr><td>2022-now</td><td>National University of Kaohsiung</td><td>Information Management</td>
+</table>
+</center>
+
+<hr width ="100%" color ="black">
+
+<h3><font size = "5" color = "black">ABOUT ME</font></h3>
+<ol>
+<li>Language Skill : English, Chinese, Indonesian
+<li>Interpersonal Skill : Ability to relate to others, teamwork, problem solvers, respectful, committed and passionate
+</ol>
+
+<hr width ="100%" color ="black">
+
+<h4><font size = "5" color = "black">CONTACT</font></h4>
+<ul>
+<li><b>Address: </b> No. 700號, Gaoxiongdaxue Rd, Nanzih District, Kaohsiung City, 811
+<li><b>Phone: </b>0908432567
+<li><b>E-mail: </b><a href="mailto:a1113339@mail.nuk.edu.tw" target="new">a1113339@mail.nuk.edu.tw</a>
+</ul>
+
+
+</body>
+
+</html> 
